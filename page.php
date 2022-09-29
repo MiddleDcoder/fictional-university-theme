f798617f1ce5c2d197dcacc4ec03cded5d4b88a4
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-09-29 11:30:48 +0800

    The Famous Loop in WordPress part2

---
+++ page.php
@@ -0,0 +1,12 @@
+<?php
+
+    while(have_posts()) {
+         the_post(); ?>
+         <h1>This is a page not a post</h1>
+        <h2><?php the_title(); ?></h2>
+        <?=the_content(); ?>
+        
+   <?php }
+
+?>
+ 
