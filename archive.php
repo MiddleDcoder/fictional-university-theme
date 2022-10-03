4d6ac902ffd42fc874b713974a7c5e5db244c308
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-03 13:09:38 +0800

    shorter archive title

--- archive.php
+++ archive.php
@@ -5,13 +5,7 @@ get_header(); ?>
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>)"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">
-        <?php if (is_category()) {
-            single_cat_title();
-           }
-           if (is_author()) {
-            echo 'Post by '; the_author();
-           }  
-        ?>
+        <?php the_archive_title();?>
         </h1>
         <div class="page-banner__intro">
           <p>Keep up with our latest news.</p>
