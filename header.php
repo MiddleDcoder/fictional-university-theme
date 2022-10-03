ef706cc64a1d2e34473bee2816eff07cd828ee6f
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-03 11:27:24 +0800

    highlighting the nav of current page and child page parent id

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
       <div class="site-header__menu group">
         <nav class="main-navigation">
           <ul class="min-list group">
-            <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+            <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 15) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
             <li><a href="#">Programs</a></li>
             <li><a href="#">Events</a></li>
             <li><a href="#">Campuses</a></li>
