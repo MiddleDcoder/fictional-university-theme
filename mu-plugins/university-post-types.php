fb0e9c115f7cba78ba6ba9fa514fad54971c136b
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-04 15:28:23 +0800

    Creating Relationships Between Content

--- mu-plugins/university-post-types.php
+++ mu-plugins/university-post-types.php
@@ -2,6 +2,7 @@
 
 function university_post_types(){
     //create new post type
+    // Event Post Type
     register_post_type('event', array(
       'supports' => array('title', 'editor', 'excerpt'),
       'rewrite' => array('slug' => 'events'),
@@ -16,6 +17,23 @@ function university_post_types(){
       ),
       'menu_icon' => 'dashicons-calendar'
     ));
+
+    // Program Post Type
+    register_post_type('program', array(
+      'supports' => array('title', 'editor'),
+      'rewrite' => array('slug' => 'programs'),
+      'has_archive' => true,
+      'public' => true,
+      'labels' => array(
+        'name' => 'Programs',
+        'add_new_item' => 'Add New Program',
+        'edit_item' => 'Edit Program',
+        'all_items' => 'All Programs',
+        'singular_name' => 'Program'
+      ),
+      'menu_icon' => 'dashicons-awards'
+    ));
+
   }
   
   add_action('init', 'university_post_types');
