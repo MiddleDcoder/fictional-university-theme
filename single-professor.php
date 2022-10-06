50d95243c548d6e7f0ef7a04dabb8071fd0763f0
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-06 17:55:39 +0800

    Featured Image Sizes _ Cropping

--- single-professor.php
+++ single-professor.php
@@ -21,7 +21,7 @@
         <div class="row group">
 
             <div class="one-third">
-            <?php the_post_thumbnail();?>
+            <?php the_post_thumbnail('professorPortrait');?>
             </div>
 
             <div class="two-thirds">
