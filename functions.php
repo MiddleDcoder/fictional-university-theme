64dea7c40a1743a4b721dc52c8dcbc0b4c2fa189
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-07 03:40:12 +0800

    added the_title and sub_title to reusable cleaner code

--- functions.php
+++ functions.php
@@ -1,5 +1,27 @@
 <?php
 
+function pageBanner($args){
+  // php logic will live here
+  if (!$args['title']) {
+  $args['title'] = get_the_title();
+  }
+
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+
+  ?>
+     <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner'] ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+        <div class="page-banner__intro">
+          <p><?php echo $args['subtitle']; ?></p>
+        </div>
+      </div>
+    </div>
+<?php }
+
 function university_files(){
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -15,6 +37,7 @@ function university_features(){
   add_theme_support('post-thumbnails'); // feature image
   add_image_size('professorLandscape', 400, 260, true);
   add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
 add_action('after_setup_theme', 'university_features');
