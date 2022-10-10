36e9f5a42446a3f87e36c404b1ebc7d4c4e3ca9e
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-10 16:16:23 +0800

    removed code filter for google api key

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
 
-function pageBanner($args = NULL){
+function pageBanner($args = NULL)
+{
   // php logic will live here
   if (!$args['title']) {
-  $args['title'] = get_the_title();
+    $args['title'] = get_the_title();
   }
 
   if (!$args['subtitle']) {
@@ -18,29 +19,30 @@ function pageBanner($args = NULL){
     }
   }
 
-  ?>
-     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
-        <div class="page-banner__intro">
-          <p><?php echo $args['subtitle']; ?></p>
-        </div>
+?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
       </div>
     </div>
+  </div>
 <?php }
 
-function university_files(){
+function university_files()
+{
   wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
-    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-    wp_enqueue_style('university_main_styles',  get_stylesheet_uri(), NULL, microtime());
-    
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  wp_enqueue_style('university_main_styles',  get_stylesheet_uri(), NULL, microtime());
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features(){
+function university_features()
+{
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails'); // feature image
   add_image_size('professorLandscape', 400, 260, true);
@@ -50,16 +52,16 @@ function university_features(){
 
 add_action('after_setup_theme', 'university_features');
 
-function university_adjust_queries($query){
+function university_adjust_queries($query)
+{
 
-  if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+  if (!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
     $query->set('orderby', 'title');
     $query->set('order', 'ASC');
     $query->set('posts_per_page', -1);
-
   }
 
-  if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
+  if (!is_admin() and is_post_type_archive('event') and $query->is_main_query()) {
     $today = date('Ymd');
     $query->set('meta_key', 'event_date');
     $query->set('orderby', 'meta_value_num');
@@ -76,3 +78,16 @@ function university_adjust_queries($query){
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
+
+
+// google map api key
+// ONLY ENABLED THIS IF YOU HAVE API KEY REGISTERED BILLING
+/*
+function universityMapKey($api)
+{
+  $api['key'] = 'APIKEY_FROM_GOOGLE_PAST_HERE';
+  return $api;
+}
+
+add_filter('acf/fields/google_map/api', 'universityMapKey');
+*/
