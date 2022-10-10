31508531a48501cc26b346d7ae3982bf9b83320d
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-10 21:50:01 +0800

    imported JS for google map ACF

--- archive-campus.php
+++ archive-campus.php
@@ -10,18 +10,17 @@ pageBanner(array(
 
 <div class="container container--narrow page-section">
 
-    <ul class="link-list min-list">
+    <div class="acf-map">
         <?php
         while (have_posts()) {
-            the_post(); ?>
-            <li><a href="<?php the_permalink(); ?>"><?php the_title();
-                                                    $mapLocation = get_field('map_location');
-                                                    echo    $mapLocation['address'];
-                                                    ?></a></li>
+            the_post();
+            $mapLocation = get_field('map_location');
+        ?>
+            <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>"></div>
         <?php  }
         echo paginate_links();
         ?>
-    </ul>
+    </div>
 
 </div>
 
