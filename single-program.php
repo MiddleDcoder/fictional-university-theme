81c0d776d030e7ba500805692f86af30a73d73de
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-11 13:46:40 +0800

    Campuses Continued: Query for related campuses

--- single-program.php
+++ single-program.php
@@ -47,7 +47,7 @@ while (have_posts()) {
             <span class="professor-card__name"><?php the_title(); ?></span>
           </a>
         </li>
-    <?php }
+      <?php }
       echo '</ul>';
     }
 
@@ -87,7 +87,21 @@ while (have_posts()) {
       }
     }
 
-    ?>
+    wp_reset_postdata();
+    $relatedCampuses = get_field('related_campus');
+
+    if ($relatedCampuses) {
+      echo '<hr class"section-break">';
+      echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available At These Campuses:</h2>';
+
+      echo '<ul class="min-list link-list">';
+      foreach ($relatedCampuses as $campus) {
+      ?> <li><a href="<?php echo get_the_permalink($campus); ?>"><?php echo get_the_title($campus) ?></a></li> <?php
+                                                                                                              }
+                                                                                                              echo '</ul>';
+                                                                                                            }
+
+                                                                                                                ?>
 
   </div>
 
