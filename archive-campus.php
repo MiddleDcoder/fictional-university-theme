dbaaf48d89444948b6a3744179801adbf31b8961
Jhun Lorenz Asna <33019593+MiddleDcoder@users.noreply.github.com>
2022-10-11 13:10:31 +0800

    created single-campus.php

--- archive-campus.php
+++ archive-campus.php
@@ -16,10 +16,11 @@ pageBanner(array(
             the_post();
             $mapLocation = get_field('map_location');
         ?>
-            <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>"></div>
-        <?php  }
-        echo paginate_links();
-        ?>
+            <div class="marker" data-lat="<?php echo $mapLocation['lat']; ?>" data-lng="<?php echo $mapLocation['lng']; ?>">
+                <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                <?php echo $mapLocation['address']; ?>
+            </div>
+        <?php } ?>
     </div>
 
 </div>
